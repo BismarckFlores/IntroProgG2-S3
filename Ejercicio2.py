c53e9d8e4682385b5c66106084c4ec78a85e17c8
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-01 11:36:16 -0600

    Ejercicio 2

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+base = float(input("¿Cuál es la base del rectángulo? "))
+altura = float(input("¿Cuál es la altura del rectángulo? "))
+area = base * altura
+perimetro = 2 * (base + altura)
+print(f"El área del rectángulo es {area} y su perimetro es {perimetro}")
